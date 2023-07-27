ef85758d1c98cc993a8625a26fec50bc2f07bbff
Bigote93 <nalli.programador@gmail.com>
2023-07-27 19:00:03 -0400

    Contador por item galeria1

--- src/component/shoppingpage/galeryproducts.js
+++ src/component/shoppingpage/galeryproducts.js
@@ -5,6 +5,7 @@ import examen20unica from '../../assets/imgs/productos/examen20unica.png';
 import examen30unica from '../../assets/imgs/productos/examen30unica.png';
 import examen20multiple from '../../assets/imgs/productos/examen20multiple.png';
 import examen30multiple from '../../assets/imgs/productos/examen30multiple.png';
+import { useState } from 'react';
 
 
 
@@ -18,6 +19,7 @@ const products = [
         imageAlt: "Front of men's Basic Tee in black.",
         price: '$1.500',
         item: '20 items',
+        quantity: 0,
     },
     {
         id: 2,
@@ -27,6 +29,7 @@ const products = [
         imageAlt: "Front of men's Basic Tee in black.",
         price: '$2.000',
         item: '30 items',
+        quantity: 0,
     },
     {
         id: 3,
@@ -36,6 +39,7 @@ const products = [
         imageAlt: "Front of men's Basic Tee in black.",
         price: '$2.500',
         item: '20 items',
+        quantity: 0,
     },
     {
         id: 4,
@@ -45,52 +49,104 @@ const products = [
         imageAlt: "Front of men's Basic Tee in black.",
         price: '$3.000',
         item: '30 items',
+        quantity: 0,
     },
 
     // More products...
 ]
 
+{/**
+    const [product, setProducts] = useState(products);
+
+    function handleAddClick(productId) {
+        setProducts((prevProducts) =>
+            prevProducts.map((product) =>
+                product.id === productId ? { ...product, quantity: product.quantity + 1 } : product
+            )
+        );
+    }
+
+    function handleRemoveClick(productId) {
+        setProducts((prevProducts) =>
+            prevProducts.map((product) =>
+                product.id === productId && product.quantity > 0
+                    ? { ...product, quantity: product.quantity - 1 }
+                    : product
+            )
+        );
+    }
+
+     */}
 
 export default function Galery1() {
-    return (
-        <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-10 py-6 sm:px-10 sm:py-6 lg:max-w-7xl lg:px-6">
-                <h2 className="text-4xl font-bold tracking-tight text-gray-900">Servicios</h2>
+    const [productQuantities, setProductQuantities] = useState(products.map(() => 0));
 
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
-                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                />
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.name}
-                                        </a>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.item}</p>
+    const handleAddClick = (productId) => {
+        setProductQuantities((prevQuantities) =>
+            prevQuantities.map((quantity, index) =>
+                index === productId - 1 ? quantity + 1 : quantity
+            )
+        );
+    };
+
+    const handleRemoveClick = (productId) => {
+        setProductQuantities((prevQuantities) =>
+            prevQuantities.map((quantity, index) =>
+                index === productId - 1 ? Math.max(quantity - 1, 0) : quantity
+            )
+        );
+    };
+
+        return (
+            <div className="bg-white">
+                <div className="mx-auto max-w-2xl px-10 py-6 sm:px-10 sm:py-6 lg:max-w-7xl lg:px-6">
+                    <h2 className="text-4xl font-bold tracking-tight text-gray-900">Productos</h2>
+                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        {products.map((product) => (
+                            <div key={product.id} className="group relative">
+                                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                    <img
+                                        src={product.imageSrc}
+                                        alt={product.imageAlt}
+                                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                    />
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                <div className="mt-4 flex justify-between">
+                                    <div>
+                                        <h3 className="text-sm text-gray-700">
+                                            <a href={product.href}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {product.name}
+                                            </a>
+                                        </h3>
+                                        <p className="mt-1 text-sm text-gray-500">{product.item}</p>
+                                    </div>
+                                    <p className="text-sm font-medium text-gray-900 justify-center">{product.price}</p>
+                                </div>
+                                <div className="pt-4 px-6 flex justify-around group-hover:opacity-75">
+                                    <button
+                                        onClick={() => handleAddClick(product.id)}
+                                        className="py-2 px-4 bg-gray-900 text-white font-semibold rounded-3xl shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+                                    >
+                                        Agregar
+                                    </button>
+                                    <button
+                                        onClick={() => handleRemoveClick(product.id)}
+                                        className="py-2 px-4 bg-gray-900 text-white font-semibold rounded-3xl shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+                                    >
+                                        Eliminar
+                                    </button>
+                                </div>
+                                <div className='pt-4 flex justify-around border-slate-700'>
+                                <p className="p-2 flex items-center  text-xl">
+                                    Agregados: {productQuantities[product.id - 1]}
+                                </p>
+                                </div>
+                                
                             </div>
-                            <div className='pt-4 px-6 flex justify-around group-hover:opacity-75'>
-                                <button className="py-2 px-4 bg-gray-900 text-white font-semibold rounded-3xl shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Agregar</button>
-                                <button className="py-2 px-4 bg-gray-900 text-white font-semibold rounded-3xl shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Eliminar</button>
-                            </div>
-                            <p className='pt-4 flex justify-around'> Cantidad </p>
-
-                        </div>
-                    ))}
-
-
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        );
+    }
